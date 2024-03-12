92985a8f1203cc083aace2dae8780692364ae68a
Janabil <zhanabil.sydykov@bcc.kz>
2024-03-13 00:17:18 +0500

    create new page and file

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:light_dark_mode/pages/home_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -6,4 +7,12 @@ void main() {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
 }
