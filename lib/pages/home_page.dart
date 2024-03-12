98f30e7ec59cc7871c99d983177fc1a68671da3f
Janabil <zhanabil.sydykov@bcc.kz>
2024-03-13 00:45:10 +0500

    create box.dart and button.dart

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,10 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:light_dark_mode/components/box.dart';
+import 'package:light_dark_mode/components/button.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.blue,
+      body: Center(
+        child: MyBox(
+          color: Colors.blue[300],
+          child: MyButton(
+            color: Colors.blue[200],
+            onTap: () {},
+          ),
+        ),
+      ),
+    );
   }
 }
